f2c97f08dbdb2dcbd4af49fa1a44038b66529d3c
JHAhmed <jamalhascientist@gmail.com>
2026-01-06 16:04:38 +0530

    feat: redesign favicon and Open Graph image, minor design improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import './layout.css';
-	import favicon from '$lib/assets/logo.ico';
+	import favicon from '$lib/assets/favicon.svg';
 	import { page } from '$app/state';
 
 	let { children } = $props();
@@ -10,13 +10,15 @@
 	<link rel="icon" href={favicon} />
 
 	<title>YANPA - Yet Another Nopepad</title>
-	<meta name="description" content="A stupidly dead simple text editor." />
+	<meta name="description" content="A stupidly dead simple text editor" />
 	<meta property="og:title" content="YANPA - Yet Another Nopepad" />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="{page.url.origin}/ogimage.jpg" />
 	<meta property="og:url" content="{page.url.origin}/" />
 	<meta property="og:description" content="A stupidly dead simple text editor." />
 </svelte:head>
+
+{@render children()}
 
 <!-- <div
 	class="flex min-h-dvh flex-col bg-gray-50 p-4 transition-all duration-300 ease-in-out selection:rounded-sm selection:bg-gray-800 selection:text-gray-100 md:p-6 lg:p-8 dark:bg-gray-900">
@@ -27,4 +29,3 @@
 		SvelteKit & ❤️
 	</p>
 </div> -->
-{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,7 +64,6 @@
 		if (browser) {
 			text = localStorage.getItem('yanpa-text') || '';
 
-			// Fix: Parse integer to avoid string concatenation errors later
 			textSize = parseInt(localStorage.getItem('yanpa-text-size') || '1');
 
 			spellcheck = localStorage.getItem('yanpa-spellcheck') === 'true';
@@ -103,9 +102,9 @@
 <div
 	class="flex min-h-dvh flex-col bg-gray-50 p-4 transition-all duration-300 ease-in-out selection:rounded-sm selection:bg-gray-800 selection:text-gray-100 md:p-6 lg:p-8 dark:bg-gray-900">
 	<div
-		class="relative flex h-full w-full grow flex-col rounded-2xl bg-white shadow-xl/5 transition-all duration-300 ease-in-out dark:bg-gray-950">
+		class="relative flex h-full w-full grow flex-col rounded-2xl p-2 lg:p-3 bg-white shadow-xl/5 transition-all duration-300 ease-in-out dark:bg-gray-950">
 		<textarea
-			class="focus:ring-none h-full w-full flex-1 resize-none p-4 tracking-[-0.015em] text-gray-800 focus:outline-none lg:p-6 dark:text-gray-200 {textSizeMap[
+			class="focus:ring-none h-full w-full flex-1 resize-none p-2 lg:p-3 tracking-[-0.015em] text-gray-800 focus:outline-none  dark:text-gray-200 {textSizeMap[
 				textSize
 			]}"
 			cols="64"
@@ -123,8 +122,8 @@
 			<button
 				onclick={toggleSpellcheck}
 				class="rounded-md {spellcheck
-					? 'bg-green-50 hover:bg-green-100 dark:bg-green-900 dark:hover:bg-green-800'
-					: 'bg-red-50 hover:bg-red-100 dark:bg-red-900 dark:hover:bg-red-800'} p-0.5 px-2 text-xs text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100">
+					? 'bg-green-100 hover:bg-green-200/90 dark:bg-green-900 dark:hover:bg-green-800/90'
+					: 'bg-red-100 hover:bg-red-200/90 dark:bg-red-900 dark:hover:bg-red-800/90'} p-0.5 px-2 text-xs text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100">
 				Spellcheck
 			</button>
 
@@ -132,7 +131,7 @@
 				<button onclick={button.action}
 					><Icon
 						icon={button.icon}
-						class="size-6 cursor-pointer rounded-md bg-gray-50 p-0.5 text-gray-600 hover:bg-gray-100  dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 " /></button>
+						class="size-6 cursor-pointer rounded-md bg-gray-100 p-0.5 text-gray-600 hover:bg-gray-200  dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 " /></button>
 			{/each}
 		</div>
 
